70ce58e675c0db0862ef9ca52ecaf99e5c5ce55b
Asim Iqbal <its.asimiqbal@gmail.com>
2020-04-22 17:15:44 +0200

    generic training
    Adding training_generic_DeNeRD.m to save images inside subfolders:
    dataset/i1
    dataset/i2
    ...

---
+++ training_generic_DeNeRD.m
@@ -0,0 +1,37 @@
+%% DeNeRD pipeline
+% Cite: Iqbal A, et al. DeNeRD: high-throughput detection of neurons for brain-wide analysis with deep learning. Scientific Reports. 2019 Sep 25; 9 (1): 1-3.
+% code to generate an initial training.mat file for Faster R-CNN
+% works for multiple folders (e.g. i1, i2) inside the dataset/ folder
+
+%% ## going inside dataset/ folder
+clc; clear all;
+data_list=string(ls('dataset')); %go inside 
+data_list(1:2) = [];
+data_list = sort_nat(data_list);
+disp('data_list: '); disp(data_list);
+disp('shape of data_list: '); disp(size(data_list));
+data_list(:,1)=strcat('dataset/', data_list(:,1));
+disp('data_list: '); disp(data_list);
+
+%% ## going inside dataset/iNs folders
+data_final = strings(); %start with an empty string
+for i=1:length(data_list)
+    data_new = strcat(data_list(i),'/', string(ls(data_list(i))));
+    data_new(1:2) = [];
+    data_new = sort_nat(data_new);
+    data_final = cat(1,data_new,data_final);
+end
+data_lim = length(data_final)-1;
+data_final = data_final(1:data_lim);
+trainingdata = array2table(data_final);
+trainingdata = table2cell(trainingdata);
+
+%% generate a sample bounding box on top left of each image for start
+for j=1:length(data_final)
+    bounds = [1, 1, 10, 10];
+    bounds3{j,1}=bounds;
+end
+s2 = horzcat(trainingdata,bounds3);
+training = cell2table(s2);
+training = table2cell(training);
+save('training_generic.mat', 'training');
